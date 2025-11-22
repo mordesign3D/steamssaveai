f6084fa26cc6589a80ecffe14e66d4a2a8821211
M O R T U T O <mortuto@ms-macbook-air.home>
2025-11-22 14:29:25 +0000

    Repair ZSH Safe Mode

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Serveur OK');
+    res.send('✅ Serveur StreamSave en ligne !');
 });
 
 app.get('/download', async (req, res) => {
@@ -13,13 +13,9 @@ app.get('/download', async (req, res) => {
         const ytdl = require('@distube/ytdl-core');
         const url = req.query.url;
         
-        // Verification sans point d'exclamation pour eviter erreur ZSH
-        if (url == null || url == '') {
-            return res.status(400).send('URL manquante');
-        }
+        if (!url) return res.status(400).send('URL manquante');
         
-        const isValid = ytdl.validateURL(url);
-        if (isValid == false) {
+        if (!ytdl.validateURL(url)) {
              return res.status(400).send('URL invalide');
         }
 
@@ -35,7 +31,7 @@ app.get('/download', async (req, res) => {
 
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur serveur');
+        res.status(500).send(error.message);
     }
 });
 
